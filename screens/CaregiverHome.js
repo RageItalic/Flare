a3e98d9cd965cf11a3723fa06e4d60adeb4453ca
Yi Fei Huang <hyf97ca@gmail.com>
2020-01-18 23:50:00 -0500

    add mapview

--- screens/CaregiverHome.js
+++ screens/CaregiverHome.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react'
-import {View, Text, StyleSheet, AsyncStorage, Button} from 'react-native'
+import {View, Text, StyleSheet, AsyncStorage, Button, Dimensions} from 'react-native'
+
+import MapView from 'react-native-maps';
 
 class CaregiverHome extends Component {
-    state = {
+      state = {
         token: ""
-    };
+      }
 
     async componentDidMount() {
         this.setState({
@@ -15,15 +17,31 @@ class CaregiverHome extends Component {
         await AsyncStorage.clear();
         this.props.navigation.navigate('Auth');
     };
-
+        /*                */
     render() {
         return (
-            <View style={{flex: 1, alignItems: "center", justifyContent: 'center'}}>
+            <View style={styles.container}>
                 <Text>Hi caregiver, {this.state.token}</Text>
                 <Button title="Actually, sign me out :)" onPress={this._signOutAsync} />
+                <MapView style={styles.mapStyle} />
+                <Text>random text</Text>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    mapStyle: {
+      flex: 1,
+      width: Dimensions.get('window').width,
+      height: Dimensions.get('window').height,
+    },
+  });
 
 export default CaregiverHome;
